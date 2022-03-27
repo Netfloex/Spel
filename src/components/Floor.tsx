fb356de3a044d7c046a62aa31bf0c472f9569a3f
Netfloex <38650595+Netfloex@users.noreply.github.com>
2022-03-27 01:13:59 +0100

    Enabled Github Actions, docker build

--- src/components/Floor.tsx
+++ src/components/Floor.tsx
@@ -3,6 +3,6 @@ import type { FC } from "react"
 export const Floor: FC = () => (
 	<mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
 		<planeBufferGeometry attach="geometry" args={[5000, 5000, 128, 128]} />
-		<meshStandardMaterial />
+		<meshStandardMaterial color={0x343434} />
 	</mesh>
 )
